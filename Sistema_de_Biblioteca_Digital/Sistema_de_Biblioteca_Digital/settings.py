14a7d24477afefd49ba1e51c367069e5a1e23836
KevDev-exe <kevinyamid2005@gmail.com>
2026-05-13 18:46:45 -0500

    creacion de la app libros, se esta realizando la creacion de categorias y autores y la vista de libros, falta ajusta los cruds de autores y categorias para asi porder agregar un libro.

--- Sistema_de_Biblioteca_Digital/Sistema_de_Biblioteca_Digital/settings.py
+++ Sistema_de_Biblioteca_Digital/Sistema_de_Biblioteca_Digital/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'libros',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_REDIRECT_URL = 'perfil'
 LOGOUT_REDIRECT_URL = 'login'
